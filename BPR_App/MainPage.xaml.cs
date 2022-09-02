e6be5503336ad8372f4e0e8eb5d9abf09925659b
Christian Sejer <christian_sejer@hotmail.com>
2022-09-02 13:13:31 +0200

    initial commit

---
+++ BPR_App/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BPR_App;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BPR_App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BPR_App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
