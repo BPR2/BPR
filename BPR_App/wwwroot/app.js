a68615e675f3bcd492be6541e7a38f597f009267
Kianoush97 <58509806+Kianoush97@users.noreply.github.com>
2022-10-11 19:36:24 +0200

    Updated UI for create account, getting map for fields.

--- BPR_App/wwwroot/app.js
+++ BPR_App/wwwroot/app.js
@@ -1,19 +1,21 @@
-﻿function changeIcon(button) {
+﻿function expandCollapse(button) {
     var icon = button.querySelector("span").querySelector("i")
     if (icon.classList.contains("fa-angle-down")) {
         icon.classList.remove("fa-angle-down")
         icon.classList.add("fa-angle-up")
 
-        var latlng = new google.maps.LatLng(long, lat);
-        var options = {
-            zoom: 2, center: latlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map"), options);
-
-    }else if (icon.classList.contains("fa-angle-up")) {
+    } else if (icon.classList.contains("fa-angle-up")) {
         icon.classList.remove("fa-angle-up")
         icon.classList.add("fa-angle-down")
 
     }
+}
+
+function getMap(long, lat) {
+    var latlng = new google.maps.LatLng(long, lat);
+    var options = {
+        zoom: 2, center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), options);
 }
