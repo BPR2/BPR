01e14e519aa992985bb79b0e915b4350cc81f4cf
KevinPongo <kpo@scadaminds.com>
2022-10-24 15:27:21 +0200

    added receiver list view

--- BPR_WebAPI/Persistence/Receivers/ReceiverRepo.cs
+++ BPR_WebAPI/Persistence/Receivers/ReceiverRepo.cs
@@ -1,5 +1,7 @@
 ﻿using BPR_RazorLibrary.Models;
+using BPR_RazorLibrary.Pages;
 using Npgsql;
+using System.Collections.Generic;
 
 namespace BPR_WebAPI.Persistence.Receivers;
 
@@ -174,4 +176,63 @@ public class ReceiverRepo : IReceiverRepo
 			return WebResponse.ContentUpdateFailure;
 		}
 	}
+
+	public async Task<WebContent> GetAllReceiversList()
+	{
+        List<Receiver> receivers = new List<Receiver>();
+
+        try
+        {
+            using var con = new NpgsqlConnection(connectionString);
+            con.Open();
+
+            string command = $"SELECT distinct on (r.serialnumber) r.receiverid, r.serialnumber, rd.timestamp, a.username\r\nFROM public.receiver r \r\nLeft JOIN public.receiverdata rd \r\nON rd.receiverid = r.receiverid\r\nLeft JOIN public.account a \r\nON r.accountid = a.accountid\r\norder by r.serialnumber, rd.timestamp desc";
+
+            await using (NpgsqlCommand cmd = new NpgsqlCommand(command, con))
+            {
+                await using (NpgsqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    while (await reader.ReadAsync())
+                    {
+                        receivers.Add(new Receiver
+                        {
+                            SerialNumber = reader["serialnumber"].ToString(),
+                            ReceiverId = int.Parse(reader["receiverid"].ToString()),
+                            ReceiverLatestData = string.IsNullOrEmpty(reader["timestamp"].ToString()) ? null : new ReceiverData() {Timestamp = DateTime.Parse(reader["timestamp"].ToString()).ToLocalTime() },
+                            Description = reader["username"].ToString()
+                        });
+					}
+			}
+
+            foreach (var receiver in receivers)
+            {
+                List<Sensor> sensors = new List<Sensor>();
+
+                string command2 = $"SELECT s.sensorid, s.tagnumber FROM public.sensor s LEFT JOIN sensormeasurement sm ON sm.sensorid = s.sensorid where s.receiverId = @ReceiverId order by timestamp desc limit (select count(*) FROM sensor where receiverid = @ReceiverId)";
+
+                await using (NpgsqlCommand cmd = new NpgsqlCommand(command2, con))
+                {
+                    cmd.Parameters.AddWithValue("@ReceiverId", NpgsqlTypes.NpgsqlDbType.Integer, receiver.ReceiverId);
+
+                    await using (NpgsqlDataReader reader = await cmd.ExecuteReaderAsync())
+                        while (await reader.ReadAsync())
+                        {
+                            sensors.Add(
+                                    new Sensor
+                                    {
+                                        SensorId = int.Parse(reader["sensorid"].ToString()),
+                                        TagNumber = reader["tagnumber"].ToString(),               
+                                    });
+                        }
+                }
+                receiver.Sensors = sensors;
+            }
+
+            con.Close();
+            return new WebContent(WebResponse.ContentRetrievalSuccess, receivers);
+        }
+        catch (Exception e)
+        {
+            return new WebContent(WebResponse.ContentRetrievalFailure, null);
+        }
+    }
 }
